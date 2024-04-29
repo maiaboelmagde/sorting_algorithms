6fa4a674c6b52bc213fe1ad20be1df35b13b28f6
mai <maishalan00000@gmail.com>
2024-04-29 03:28:43 +0300

    The 0th task solution

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function that sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ *
+ * @array: Our array
+ * @size: array size
+ *
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	bool swapped;
+
+	for (i = 0; i < size - 1 ; i++)
+	{
+		swapped = false;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int temp = array[j];
+
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = true;
+				print_array(array, size);
+			}
+		}
+		if (swapped == false)
+		{
+			break;
+		}
+
+	}
+}
--- sort.h
+++ sort.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -14,12 +15,13 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
 
 #endif
